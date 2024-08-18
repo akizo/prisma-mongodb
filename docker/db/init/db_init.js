5b122fa547ecf5171648f257adf95f0ee09f61f5
akizo <ug.akimoto@gmail.com>
2024-08-18 23:19:30 +0900

    replicattion mongodb

---
+++ docker/db/init/db_init.js
@@ -0,0 +1,33 @@
+init = false;
+print("Init script ...")
+
+try {
+  if (!db.isMaster().ismaster) {
+    print("Error: primary not ready, initialize ...")
+    rs.initiate(
+      {
+        _id:'rs0',
+        members: [
+          { _id:0,
+            host: "mongo:27017"
+          }
+        ]
+      }
+    )
+    quit(1);
+  } else {
+    if (!init) {
+      admin = db.getSiblingDB("admin");
+      admin.createUser(
+        {
+          user: "root",
+          pwd: "password",
+          roles: ["readWriteAnyDatabase"]
+        }
+      );
+      init = true;
+    }
+  }
+} catch(e) {
+  rs.status().ok
+}
--- test/db.e2e-spec.ts
+++ test/db.e2e-spec.ts
@@ -1,21 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import { AppModule } from '../src/app.module';
 import { PrismaClient, Role, User } from '@prisma/client';
 
 const prisma: PrismaClient = new PrismaClient();
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
 
   it('/ (GET)', async () => {
     await prisma.user.create({
